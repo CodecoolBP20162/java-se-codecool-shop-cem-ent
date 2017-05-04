0d0a13c11d5a2da1b776028a4c437a81fd3e7b3d
Fanni Skoda <skoda.fanni@gmail.com>
2017-05-04 23:36:46 +0200

    code cleanup

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,13 +1,7 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.CartDao;
-import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.CartDaoMem;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Cart;
 import com.codecool.shop.model.LineItem;
 import org.json.simple.JSONObject;
@@ -16,16 +10,13 @@ import spark.Request;
 import spark.Response;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class CartController {
 
     public static JSONObject addItemToCart(Request req, Response res) {
-
         int addedProductId = Integer.parseInt(req.params(":id"));
         ProductDao productDataStore = ProductDaoMem.getInstance();
-
         Cart cartDataStore = getCart(req);
 
         LineItem lineItemCandidate = new LineItem(productDataStore.find(addedProductId));
@@ -35,24 +26,15 @@ public class CartController {
         jsonObj.put("numberOfProductsInCart", cartDataStore.getAllQuantity());
         res.type("application/json");
         return jsonObj;
-
-
     }
-
 
     public static ModelAndView renderCart(Request req, Response res) {
         Cart cartDataStore = getCart(req);
-
         Map<String, Object> params = new HashMap<>();
         params.put("lineitems", cartDataStore.getAll());
         params.put("sum", cartDataStore.getSum());
-
         return new ModelAndView(params, "cartview");
     }
-
-
-
-
 
     public static Cart getCart(Request req) {
         Cart cart = req.session().attribute("cart");
