2a44a99f1008755632bcc8fbcaaedbaaaf037fa9
kristof.rado@gmail.com <kristof.rado@gmail.com>
2017-05-15 13:11:28 +0200

    Working login, postgres sql dependency added.

---
+++ classes/production/my-app/public/js/main.js
@@ -0,0 +1,39 @@
+/**
+ * Created by szarazdenes on 2017. 05. 03..
+ */
+
+
+function cartAdder() {
+
+    $(".add-to-cart").click(function insertToCart() {
+        var idToCart = this.getAttribute("id");
+        var pureId = idToCart.slice(8);
+        var idProd = JSON.parse(pureId);
+
+        toSession(idProd);
+    })
+
+}
+
+function toSession(idProd) {
+    $.ajax({
+        url: '/addtocart/' + idProd,
+        type: 'GET',
+        data: 'json',
+        success: function (data) {
+            var obj = data["numberOfProductsInCart"];
+            $("#cart-counter").html(obj);
+        },
+        error: function () {
+            console.log("Error")
+        }
+    });
+
+}
+
+$(document).ready(function () {
+
+    cartAdder();
+
+
+});
