fb73e81f8b7ec7a568df42593c86ec4fd4268462
kristof.rado@gmail.com <kristof.rado@gmail.com>
2017-05-16 14:24:05 +0200

    SQL file corrected. Drop issue fixed. foreign key issue resolved.

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -1,18 +1,19 @@
+DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS suppliers;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS productcategory;
-DROP TABLE IF EXISTS productdatastore;
+
 
 CREATE TABLE supplier
 (
-id varchar(36) PRIMARY KEY,
+id int PRIMARY KEY,
 name varchar(80),
 description varchar(250)
 );
 
 CREATE TABLE users
 (
-id varchar(36) PRIMARY KEY,
+id int PRIMARY KEY,
 name varchar(40),
 password varchar(60),
 rank int
@@ -20,19 +21,21 @@ rank int
 
 CREATE TABLE productcategory
 (
-id varchar(36) PRIMARY KEY,
+id int PRIMARY KEY,
 name varchar(40),
 department varchar(10),
 description varchar(200)
 );
 
-CREATE TABLE productdatastore
+CREATE TABLE product
 (
-id varchar(36) PRIMARY KEY,
+id int PRIMARY KEY,
 name varchar(40),
 price int,
 currency VARCHAR (4),
 description VARCHAR (200),
+supplier int,
+productcategory int,
 FOREIGN KEY (supplier) REFERENCES supplier(id),
-FOREIGN KEY (productcategors) REFERENCES productcategory(id)
+FOREIGN KEY (productcategory) REFERENCES productcategory(id)
 );
