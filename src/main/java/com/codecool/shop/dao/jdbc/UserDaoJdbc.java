1c6162475608d0465eef0034865d5485da0e5c88
kristof.rado@gmail.com <kristof.rado@gmail.com>
2017-05-17 11:39:54 +0200

    User JDBC dao SELECT query implementation attempt.

--- src/main/java/com/codecool/shop/dao/jdbc/UserDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/jdbc/UserDaoJdbc.java
@@ -4,17 +4,22 @@ import com.codecool.shop.DbConnection;
 import com.codecool.shop.dao.UserDao;
 import com.codecool.shop.model.User;
 
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoJdbc implements UserDao {
     @Override
     public void add(User user) {
-        String userid = "SELECT id FROM users ORDER BY id DESC LIMIT 1";
+        String userId = "SELECT id FROM users ORDER BY id DESC LIMIT 1";
+
+        DbConnection.executeQuery(userId);
+
         //get last element from DB and increment it.
         user.setId(DATA.size() + 1);
         String query = "INSERT INTO users (name, password, rank) " +
                 "VALUES ('" + user.getName() + "', '" + user.getPassword() + "', '" + user.getRank() + "');";
-        DbConnection.executeQuery(query);
     }
 
     @Override
