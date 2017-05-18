06daf04a8eab7bf7924ea6af08037d64e325343f
Fanni Skoda <skoda.fanni@gmail.com>
2017-05-18 16:14:45 +0200

    supplier test for jdbc

--- test/com/codecool/shop/dao/SupplierDaoTest.java
+++ test/com/codecool/shop/dao/SupplierDaoTest.java
@@ -1,7 +1,9 @@
 package com.codecool.shop.dao;
 
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.dao.jdbc.SupplierDaoJdbc;
 import com.codecool.shop.model.Supplier;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,11 +15,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class SupplierDaoTest {
 
     private static SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+    //private static SupplierDao supplierDataStore = SupplierDaoJdbc.getInstance();
     private static Supplier amazon = new Supplier("Amazon", "Digital content and services");
     private static Supplier lenovo = new Supplier("Lenovo", "Computers");
 
     @BeforeEach
     void initializeTestRequirements(){
+
         List<Integer> supplierIdList = new ArrayList<>();
         for (Supplier supplier: supplierDataStore.getAll()) {
             supplierIdList.add(supplier.getId());
@@ -29,15 +33,16 @@ class SupplierDaoTest {
 
     @Test
     void testAddNewSupplier(){
+        int size = supplierDataStore.getAll().size();
         supplierDataStore.add(amazon);
-        assertEquals(amazon, supplierDataStore.getAll().get(0));
+        assertEquals(amazon.getId(), supplierDataStore.getAll().get(size).getId());
     }
 
     @Test
     void testFindSupplier(){
-        supplierDataStore.add(amazon);
+        int size = supplierDataStore.getAll().size();
         supplierDataStore.add(lenovo);
-        assertEquals(lenovo, supplierDataStore.find(2));
+        assertEquals(lenovo.getName(), supplierDataStore.find(size).getName());
     }
 
     @Test
