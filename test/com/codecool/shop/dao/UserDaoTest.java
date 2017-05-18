f98caca7e59b5c724b55bf6866db0362df203721
kristof.rado@gmail.com <kristof.rado@gmail.com>
2017-05-18 10:27:06 +0200

    userDaoJdbc test first draft.

--- test/com/codecool/shop/dao/UserDaoTest.java
+++ test/com/codecool/shop/dao/UserDaoTest.java
@@ -1,6 +1,6 @@
 package com.codecool.shop.dao;
 
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.UserDaoMem;
 import com.codecool.shop.dao.jdbc.UserDaoJdbc;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.User;
@@ -14,12 +14,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoTest {
 
+    static UserDao userDataStore = UserDaoMem.getInstance();
+
+    @BeforeEach
+    public void initializeTestRequirements(){
+        List<Integer> productIdList = new ArrayList<>();
+        for (User user : userDataStore.getAll()) {
+            productIdList.add(user.getId());
+        }
+        for (Integer id: productIdList){
+            userDataStore.remove(id);
+        }
+    }
+
     @Test
-    public void testAddNewProductCategory(){
+    public void testAddNewUser(){
         UserDao userDataStore = UserDaoJdbc.getInstance();
         User admin = new User("admin", "admin", 1);
+        admin.setId(1);
         userDataStore.add(admin);
         assertEquals(admin.toString(), userDataStore.getAll().get(0).toString());
     }
 
+    //needs to be looked at
+    @Test
+    public void testFindProductCategory(){
+        User user = new User("user", "pass", 3);
+        userDataStore.add(user);
+        assertEquals(1, userDataStore.getAll().size());
+    }
+
+    @Test
+    public void testRemoveProductCategory(){
+        User user = new User("admin2", "admin2", 1);
+        userDataStore.add(user);
+        userDataStore.remove(1);
+        assertEquals(0, userDataStore.getAll().size());
+    }
+
+    @Test
+    public void testGetAllProductCategory(){
+        List<ProductCategory> productCategories = new ArrayList<>();
+        assertEquals(productCategories, userDataStore.getAll());
+    }
 }
